6414092c5ea380d17392e65477de59fd67b447d4
Ethan <598943251@qq.com>
2017-11-10 23:01:55 +0800

    implements Yui's skill(onDraw and checkInit)
    Signed-off-by: Ethan <598943251@qq.com>

--- girl/girls_other.cpp
+++ girl/girls_other.cpp
@@ -37,8 +37,34 @@ void Yui::onDraw(const Table &table, Mount &mount, Who who, bool rinshan)
     if (who != mSelf || rinshan)
         return;
 
-    // TODO implement
-    (void) table; (void) mount;
+    const Hand &hand = table.getHand(who);
+    if (hand.barks().size() > 0)
+        return;
+    const int s4 = hand.step4();
+    const int s7 = hand.step7();
+    int mk = 400;
+
+    if (s4 <= 0 || (s4 <= 3 && s7 > s4 + 1))
+        return;
+    if (s4 <= 3 && s7 > s4)
+        mk = 150;
+    else if (s4 == 1 && s7 <= 2)
+        mk = 80;
+    for (T34 t : tiles34::ALL34)
+        if (hand.hasEffA7(t))
+            mount.lightA(t, mk, false);
+
+}
+
+bool Yui::checkInit(Who who, const Hand &init, const Princess &princess, int iter)
+{
+    if (who != mSelf || iter > 100)
+        return true;
+
+    const int s4 = init.step4();
+    const int s7 = init.step7();
+
+    return s4 <= 3 || s7 <= 3 || s7 < s4;
 }
 
 
