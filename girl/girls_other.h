7107fc3b6491295d8956cf95178133a3dc29a8c3
kangzq0530 <32447420+kangzq0530@users.noreply.github.com>
2017-11-13 10:55:56 +0800

    add 990024 (#22)
    * implement yui (onDraw and checkInit)
    
    Signed-off-by: Ethan <598943251@qq.com>
    
    * change mk value, give more chance to normal hand

--- girl/girls_other.h
+++ girl/girls_other.h
@@ -33,6 +33,8 @@ class Yui : public Girl
 public:
     GIRL_CTORS(Yui)
     void onDraw(const Table &table, Mount &mount, Who who, bool rinshan) override;
+    bool checkInit(Who who, const Hand &init, const Princess &princess, int iter) override;
+
 };
 
 
