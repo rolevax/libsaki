bb9b199476ded45d4377ff587caca0990e8fe680
rolevax <rolevax@gmail.com>
2017-11-13 16:34:23 +0800

    add util/version.h

---
+++ util/version.h
@@ -0,0 +1,21 @@
+#ifndef SAKI_VERSION_H
+#define SAKI_VERSION_H
+
+
+
+namespace saki
+{
+
+
+
+static const char *VERSION = "0.9.0-alpha1";
+
+
+
+} // namespace saki
+
+
+
+#endif // SAKI_VERSION_H
+
+
